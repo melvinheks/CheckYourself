b891c3969452e2cb0eaff6cb2e67be11ea0f0a42
chrisincode <chc9511@gmail.com>
2017-10-14 20:31:22 -0400

    testing db2

--- server/test.php
+++ server/test.php
@@ -1,26 +1,25 @@
 <?php
 echo "hello world";
-//    $hostname = "fdb7.awardspace.net";
-//    $dbname = "nawars";
-//    $uname = "chcmcmlyl";
-//    $pword = "sqlwars123";
-//
-//    $conn = new PDO("mysql:host=".$hostname.";dbname=".$dbname,$uname,$pword);
-//    $hotcmd ="CREATE TABLE MyGuests (
-//                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//                firstname VARCHAR(30) NOT NULL,
-//                lastname VARCHAR(30) NOT NULL,
-//                email VARCHAR(50),
-//                reg_date TIMESTAMP
-//                )";
-//
-//    $statement =$conn -> prepare($logcmd);
-//    if($statement->execute()){
-//        echo "it hella work";
-//    }
-//    else{
-//        echo "did not work";
-//    }
+    $hostname = "ip-172-31-18-79";
+    $dbname = "nawars";
+    $uname = "chcmcmlyl";
+    $pword = "sqlwars123";
+
+    $conn = new PDO("mysql:host=".$hostname.";dbname=".$dbname,$uname,$pword);
+    $cmd ="CREATE TABLE MyGuests (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(30) NOT NULL,
+                text VARCHAR(50) NOT NULL,
+                reg_date TIMESTAMP
+                )";
+
+    $statement =$conn -> prepare($cmd);
+    if($statement->execute()){
+        echo "it hella work";
+    }
+    else{
+        echo "did not work";
+    }
 //$logginginfo = $statement-> fetchAll(PDO::FETCH_ASSOC);    
 //	$logname=$logginginfo[0]["Fname"]." ".$logginginfo[0]["Lname"];
 //				$rows = $statement -> rowCount();
