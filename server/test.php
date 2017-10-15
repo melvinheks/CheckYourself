7d993a5d8a18582f65814625626ddc7d33a1fa08
chrisincode <chc9511@gmail.com>
2017-10-14 20:19:27 -0400

    testing db

---
+++ server/test.php
@@ -0,0 +1,44 @@
+<?php
+
+    $hostname = "fdb7.awardspace.net";
+    $dbname = "nawars";
+    $uname = "chcmcmlyl";
+    $pword = "sqlwars123";
+
+    $conn = new PDO("mysql:host=".$hostname.";dbname=".$dbname,$uname,$pword);
+    $hotcmd ="CREATE TABLE MyGuests (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                firstname VARCHAR(30) NOT NULL,
+                lastname VARCHAR(30) NOT NULL,
+                email VARCHAR(50),
+                reg_date TIMESTAMP
+                )";
+
+    $statement =$conn -> prepare($logcmd);
+    if($statement->execute()){
+        echo "it hella work";
+    }
+    else{
+        echo "did not work";
+    }
+//$logginginfo = $statement-> fetchAll(PDO::FETCH_ASSOC);    
+//	$logname=$logginginfo[0]["Fname"]." ".$logginginfo[0]["Lname"];
+//				$rows = $statement -> rowCount();
+//				if($rows > 0)
+//				{
+//					$_SESSION["uname"]=$userlogin;
+//				}
+//				else
+//				{
+//					unset($_POST['userlogin']);
+//					header("Location: Chieh-Huang Chen WIP Login.php");
+//					$_SESSION["loginfailed"] = "wrong";
+//					die();
+//				}
+//			}
+//			if(isset($_POST["loggingout"]))
+//			{
+//				unset($_SESSION["uname"]);
+//				unset($_POST['logout']);
+//			}
+?>
